216c59178af80168a8954dabe171d39f23aa499b
Liliana Sequeira <lilianasrsequeira@gmail.com>
2017-05-16 12:20:05 +0100

    Rails Wikinimous

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,44 @@
+class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def edit
+  end
+
+  def create
+    @article = Article.create(article_params)
+    redirect_to articles_path
+  end
+
+  def update
+    @article.update(article_params)
+    redirect_to articles_path
+  end
+
+  def new
+    @article= Article.new
+  end
+
+  def show
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+end
+
